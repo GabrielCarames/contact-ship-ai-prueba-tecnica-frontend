e029eedf41f1994a67b4bf5cd1f1dd5650faec20
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2024-05-10 16:04:53 -0300

    feat: add colors palette

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,21 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+      colors: {
+        lightblue: "#0074d9",
+        pink: "#d90074",
+        "black-1": "#0E1217",
+        "black-2": "#1C1F26"
+      }
+    }
   },
-  plugins: [],
-};
-export default config;
+  plugins: []
+}
+export default config
