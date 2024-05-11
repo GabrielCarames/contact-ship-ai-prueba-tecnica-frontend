3227f0930e65ccf5c116b55b79e83d670ac9f02d
GabrielCarames <49535753+GabrielCarames@users.noreply.github.com>
2024-05-10 21:12:49 -0300

    feat: add Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image"
+import Link from "next/link"
+
+export default function Footer() {
+  return (
+    <footer>
+      <p className="text-white flex text-sm items-center text-center">
+        Hecho por
+        <Link
+          href="https://www.linkedin.com/in/gabrielcarames/"
+          target="_blank"
+          className="ml-1 flex items-center gap-1 hover:text-lightblue"
+        >
+          <Image
+            src="/media/linkedin-logo.webp"
+            alt="Logo de LinkedIn"
+            width={20}
+            height={20}
+            className="mx-1"
+          />
+          Gabriel Caramés
+        </Link>
+      </p>
+    </footer>
+  )
+}
